a27d5b6fb7c5acb823744cb9bb087b36ed4e37b0
Lobo Varghese <lobo.dewdrops@gmail.com>
2021-03-08 08:56:06 +0530

    First commit and push

---
+++ sample/src/test/java/TestMaven/Checkmaven.java
@@ -0,0 +1,14 @@
+package TestMaven;
+
+import org.testng.annotations.Test;
+
+//import org.testng.annotations.Test;
+
+public class Checkmaven {
+	
+	@Test
+	public void Test1(){
+		System.out.println("This is Test1");
+	}
+
+}
